4427ddd7f3ccf463aa73686ad572f7f2806a710d
Will Angell-James <william.angelljames9@gmail.com>
2018-07-15 07:25:03 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './hello.js'
+
+
+ReactDOM.render(
+    <h1>HelloWorld</h1>,
+    document.getElementById('root')
+);
+
